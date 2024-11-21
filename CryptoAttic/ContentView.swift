8eb4253056657d0dbb6519457a36b756b8a310a0
tanmayxx12 <troy12rollsza@gmail.com>
2024-11-21 14:45:10 +0530

    Initial Commit

---
+++ CryptoAttic/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CryptoAttic
+//
+//  Created by Tanmay . on 21/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CryptoAttic/CryptoAtticApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAtticApp.swift
+//  CryptoAttic
+//
+//  Created by Tanmay . on 21/11/24.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAtticApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
